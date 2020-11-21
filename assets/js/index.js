b50a0700ee7e2c863e4be7548e79f73a496cc53d
Carlos del Prado <carlos.delprado@11paths.com>
2020-11-21 11:43:23 +0000

    feat: support mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,34 @@
+
+function onKeyEvent(event, game) {
+  if (game.drawIntervalId) {
+    game.onKeyEvent(event)
+  } else {
+    const startBtn = document.getElementById('restart-btn');
+    startBtn.classList.toggle('hidden');
+    game.restart()
+  }
+}
+
 window.addEventListener('load', () => {
-  const startBtn = document.getElementById('restart-btn');
-  
+
   const game = new Game('canvas-game', () => {
+    const startBtn = document.getElementById('restart-btn');
     startBtn.classList.toggle('hidden');
   });
   game.start();
 
-  document.addEventListener('keydown', (event) => {
-    if (game.drawIntervalId) {
-      game.onKeyEvent(event)
-    } else {
-      startBtn.classList.toggle('hidden');
-      game.restart()
-    }
+  document.addEventListener('touchstart', event => {
+    event.type = 'keydown';
+    event.keyCode = KEY_UP;
+    onKeyEvent(event, game);
   });
-  document.addEventListener('keyup', (event) => game.onKeyEvent(event));
+
+  document.addEventListener('touchend', event => {
+    event.type = 'keyup';
+    event.keyCode = KEY_UP;
+    onKeyEvent(event, game);
+  });
+
+  document.addEventListener('keydown', event => onKeyEvent(event, game));
+  document.addEventListener('keyup', event => onKeyEvent(event, game));
 });
